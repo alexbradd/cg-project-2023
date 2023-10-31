e6254e2e9075befc4dd3ea51a5e3d74264af28a2
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-10-31 17:02:29 +0100

    refactor(seng): cleanup seng::log

--- seng/include/seng/log.hpp
+++ seng/include/seng/log.hpp
@@ -7,26 +7,30 @@
 
 namespace seng::log {
 
-#define INFO_LVL "[INFO] "
-#define WARN_LVL "[WARN] "
-#define ERRO_LVL "[ERRO] "
+#define INFO_STR "[INFO] "
+#define WARN_STR "[WARN] "
+#define ERRO_STR "[ERRO] "
 
-template <typename... Args>
-void print_to_err(const std::string &lvl, const std::string &fmt,
-                  Args&&... args) {
-  std::cerr << lvl << fmt::format(fmt, args...) << std::endl;
+#ifndef NDEBUG
+#define DBUG_STR "[DBUG] "
+template <typename... Args> void dbg(const std::string &fmt, Args&&... args) {
+  std::cerr << DBUG_STR << __FILE__ << ":" << __LINE__ << " "
+            << fmt::format(fmt, args...) << std::endl;
 }
+#else
+template <typename... Args> void dbg(const std::string &fmt, Args&&... args) {}
+#endif
 
 template <typename... Args> void info(const std::string &fmt, Args&&... args) {
-  print_to_err(INFO_LVL, fmt, args...);
+  std::cerr << INFO_STR << fmt::format(fmt, args...) << std::endl;
 }
 
 template <typename... Args> void warning(const std::string &fmt, Args&&... args) {
-  print_to_err(WARN_LVL, fmt, args...);
+  std::cerr << WARN_STR << fmt::format(fmt, args...) << std::endl;
 }
 
 template <typename... Args> void error(const std::string &fmt, Args&&... args) {
-  print_to_err(ERRO_LVL, fmt, args...);
+  std::cerr << ERRO_STR << fmt::format(fmt, args...) << std::endl;
 }
 
 } // seng::log
