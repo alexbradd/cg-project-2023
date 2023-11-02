08a7662a0de713428d5150f51ae4ce1a049e97b5
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-11-02 22:09:22 +0100

    docs(seng): document seng::log

--- seng/include/seng/log.hpp
+++ seng/include/seng/log.hpp
@@ -14,26 +14,44 @@ namespace seng::log {
 
 #ifndef NDEBUG
 #define DBUG_STR "[DBUG] "
+
+/**
+ * Print a debug message to stderr. If built in release mode function is a noop.
+ * Supports fmt-style format arguments.
+ */
 template <typename... Args>
 void dbg(const std::string &fmt, Args &&...args) {
   std::cerr << DBUG_STR << __FILE__ << ":" << __LINE__ << " "
             << fmt::format(fmt, args...) << std::endl;
 }
 #else
+/**
+ * Print a debug message to stderr. If built in release mode function is a noop.
+ * Supports fmt-style format arguments.
+ */
 template <typename... Args>
 void dbg(const std::string &fmt, Args &&...args) {}
 #endif
 
+/**
+ * Print a information message to stderr. Supports fmt-style format arguments.
+ */
 template <typename... Args>
 void info(const std::string &fmt, Args &&...args) {
   std::cerr << INFO_STR << fmt::format(fmt, args...) << std::endl;
 }
 
+/**
+ * Print a warning message to stderr. Supports fmt-style format arguments.
+ */
 template <typename... Args>
 void warning(const std::string &fmt, Args &&...args) {
   std::cerr << WARN_STR << fmt::format(fmt, args...) << std::endl;
 }
 
+/**
+ * Print an error message to stderr. Supports fmt-style format arguments.
+ */
 template <typename... Args>
 void error(const std::string &fmt, Args &&...args) {
   std::cerr << ERRO_STR << fmt::format(fmt, args...) << std::endl;
