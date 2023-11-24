eae4bb5fe8628c030c2f4c4258eb88b2b5b0bdbe
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-11-24 11:43:38 +0100

    build(froggo): compile shaders and copy models

--- froggo/main.cpp
+++ froggo/main.cpp
@@ -1,11 +1,22 @@
 #include <cstdlib>
 #include <exception>
+#include <filesystem>
 #include <iostream>
 #include <seng/application.hpp>
 #include <seng/log.hpp>
 
+using namespace std;
+namespace fs = std::filesystem;
+
 int main(int argc, char* argv[]) {
+  fs::path dir{fs::path{argv[0]}.parent_path()};
+
   seng::Application app{"Froggo", 800, 600};
+
+  app.setModelPath((dir / "models").string());
+  app.setShaderPath((dir / "shaders").string());
+  seng::log::info("Reading models from {}", app.getModelPath());
+  seng::log::info("Reading shaders from {}", app.getShaderPath());
 
   try {
     seng::log::info("Starting application");
