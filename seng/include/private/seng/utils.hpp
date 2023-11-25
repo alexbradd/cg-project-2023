25075dcb8c8af7019d33cd6b979975b308d4be7e
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-11-25 17:36:31 +0100

    feat(seng): add readFile utility

---
+++ seng/include/private/seng/utils.hpp
@@ -0,0 +1,13 @@
+#ifndef __PRIVATE_SENG_UTILS_HPP__
+#define __PRIVATE_SENG_UTILS_HPP__
+
+#include <string>
+#include <vector>
+
+namespace seng::internal {
+
+extern std::vector<char> readFile(const std::string& name);
+
+} //seng::internal
+
+#endif // __PRIVATE_SENG_UTILS_HPP__
---
+++ seng/src/utils.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <seng/utils.hpp>
+
+using namespace std;
+
+vector<char> seng::internal::readFile(const string& filename) {
+  ifstream file(filename, ios::ate | ios::binary);
+  if (!file.is_open()) throw std::runtime_error("failed to open file!");
+
+  size_t fileSize = (size_t) file.tellg();
+  vector<char> buffer(fileSize);
+  file.seekg(0);
+  file.read(buffer.data(), fileSize);
+  file.close();
+
+  return buffer;
+}
