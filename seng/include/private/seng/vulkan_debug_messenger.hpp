245e82520e2a720c0b3cc0ea8d33083477ee9a80
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-11-29 01:44:23 +0100

    refactor(DebugMessenger): extract into own class

---
+++ seng/include/private/seng/vulkan_debug_messenger.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <memory>
+#include <vulkan/vulkan_raii.hpp>
+
+namespace seng::rendering {
+
+/**
+ * Class that holds a reference the DebugUtilMessengerEXT object. Intantiating
+ * it will allocate a DebugUtilMessengerEXT, while destroying it will destroy
+ * the underlying structure.
+ */
+class DebugMessenger {
+ public:
+  DebugMessenger(vk::raii::Instance &instance, bool allocate = true);
+
+  static vk::DebugUtilsMessengerCreateInfoEXT createInfo();
+
+ private:
+  std::unique_ptr<vk::raii::DebugUtilsMessengerEXT> debugMessenger;
+
+  static VKAPI_ATTR VkBool32 VKAPI_CALL
+  debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                VkDebugUtilsMessageTypeFlagsEXT messageType,
+                const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+                void *pUserData);
+};
+
+}  // namespace seng::rendering
---
+++ seng/src/vulkan_debug_messenger.cpp
@@ -0,0 +1,48 @@
+#include <memory>
+#include <seng/log.hpp>
+#include <seng/vulkan_debug_messenger.hpp>
+
+using namespace std;
+using namespace seng::rendering;
+using namespace vk::raii;
+
+DebugMessenger::DebugMessenger(Instance &instance, bool allocate)
+    : debugMessenger() {
+  if (allocate)
+    debugMessenger =
+        make_unique<DebugUtilsMessengerEXT>(instance, createInfo());
+}
+
+vk::DebugUtilsMessengerCreateInfoEXT DebugMessenger::createInfo() {
+  using Severity = vk::DebugUtilsMessageSeverityFlagBitsEXT;
+  using Type = vk::DebugUtilsMessageTypeFlagBitsEXT;
+
+  vk::DebugUtilsMessengerCreateInfoEXT ci{};
+  vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(
+      Severity::eVerbose | Severity::eWarning | Severity::eError);
+  vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags(
+      Type::eGeneral | Type::ePerformance | Type::eValidation);
+  ci.messageSeverity = severityFlags;
+  ci.messageType = messageTypeFlags;
+  ci.pfnUserCallback = debugCallback;
+  return ci;
+}
+
+VkBool32 DebugMessenger::debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT,
+    const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+    void *) {
+  switch (messageSeverity) {
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+      log::info("Validation layer: {}", pCallbackData->pMessage);
+      break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+      log::warning("Validation layer: {}", pCallbackData->pMessage);
+      break;
+    default:
+      log::error("Validation layer: {}", pCallbackData->pMessage);
+  }
+  return VK_FALSE;
+}
