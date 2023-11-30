7ece5123b06e678e92ec531d39af3b2895bc5bef
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-11-30 16:55:04 +0100

    style(utils): fix witespace

--- seng/src/utils.cpp
+++ seng/src/utils.cpp
@@ -7,7 +7,7 @@ vector<char> seng::internal::readFile(const string& filename) {
   ifstream file(filename, ios::ate | ios::binary);
   if (!file.is_open()) throw std::runtime_error("failed to open file!");
 
-  size_t fileSize = (size_t) file.tellg();
+  size_t fileSize = (size_t)file.tellg();
   vector<char> buffer(fileSize);
   file.seekg(0);
   file.read(buffer.data(), fileSize);
