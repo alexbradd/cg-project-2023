ec4c54ff5e14c8e9c6c6f9a465bec5356d1c6c8f
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-12-13 14:55:52 +0100

    style(QueueFamilyIndices): re-format to new style

--- seng/src/vulkan_queue_family_indices.cpp
+++ seng/src/vulkan_queue_family_indices.cpp
@@ -4,21 +4,20 @@ using namespace vk::raii;
 using namespace seng::rendering;
 using namespace std;
 
-QueueFamilyIndices::QueueFamilyIndices(PhysicalDevice &dev,
-                                       SurfaceKHR &surface) {
-  vector<vk::QueueFamilyProperties> queueFamilies =
-      dev.getQueueFamilyProperties();
+QueueFamilyIndices::QueueFamilyIndices(PhysicalDevice &dev, SurfaceKHR &surface)
+{
+  vector<vk::QueueFamilyProperties> queueFamilies = dev.getQueueFamilyProperties();
 
   int i = 0;
   for (const auto &familyProperties : queueFamilies) {
-    if (familyProperties.queueFlags & vk::QueueFlagBits::eGraphics)
-      _graphicsFamily = i;
+    if (familyProperties.queueFlags & vk::QueueFlagBits::eGraphics) _graphicsFamily = i;
     if (dev.getSurfaceSupportKHR(i, *surface)) _presentFamily = i;
     if (isComplete()) break;
     ++i;
   }
 }
 
-bool QueueFamilyIndices::isComplete() {
+bool QueueFamilyIndices::isComplete()
+{
   return _graphicsFamily.has_value() && _presentFamily.has_value();
 }
