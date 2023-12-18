alexbradd/cg-project-2023

commit 0ad8f81a15159654fc0819db635eaa3ce98d31f8

feat(Renderer): add descriptorSet allocation and tracking

seng/include/seng/rendering/renderer.hpp
@@ -11,6 +11,7 @@
 #include <seng/rendering/image.hpp>
 #include <seng/rendering/render_pass.hpp>
 #include <seng/rendering/swapchain.hpp>
+#include <seng/utils.hpp>
 
 #include <glm/mat4x4.hpp>
 #include <glm/vec3.hpp>
@@ -18,7 +19,22 @@
 
 #include <array>
 #include <cstdint>
+#include <unordered_map>
 #include <vector>
+
+// Copied from Vulkan-Samples. Needed so that we can put a DescriptorSetLayout handle in a
+// hashmap
+namespace std {
+template <>
+struct hash<vk::DescriptorSetLayout> {
+  std::size_t operator()(const vk::DescriptorSetLayout &descriptorSetLayout) const
+  {
+    std::size_t result = 0;
+    seng::internal::hashCombine(result, descriptorSetLayout);
+    return result;
+  }
+};
+}  // namespace std
 
 namespace seng::rendering {
 
@@ -77,10 +93,30 @@ class Renderer {
   void signalResize();
 
   /**
+   * Allocate a new descriptor set with the given layout from the pool.
+   */
+  void requestDescriptorSet(vk::DescriptorSetLayout layout);
+
+  /**
+   * Destroy all allocated descriptor sets and clear the pool.
+   */
+  void clearDescriptorSets();
+
+  /**
    * Starts recording a frame and returns a handle to it. If recording cannot be
    * started, return an empty optional.
    */
   std::optional<FrameHandle> beginFrame();
+
+  /**
+   * Fetches a descriptor set with the given layout usable during the current in-progress
+   * frame.
+   *
+   * Note: the layout must have been previously registered with a call to
+   * registerDescriptorLayout().
+   */
+  const vk::raii::DescriptorSet &getDescriptorSet(const FrameHandle &frame,
+                                                  vk::DescriptorSetLayout layout) const;
 
   void updateGlobalState(glm::mat4 projection, glm::mat4 view) const;
   void updateModel(glm::mat4 model) const;
@@ -124,10 +160,13 @@ class Renderer {
     vk::raii::Semaphore imageAvailableSem;
     vk::raii::Semaphore queueCompleteSem;
     Fence inFlightFence;
+    std::unordered_map<vk::DescriptorSetLayout, vk::raii::DescriptorSet> descriptorSets;
     ssize_t imageIndex;
 
     Frame(const Device &device, const vk::raii::CommandPool &commandPool);
   };
+
+  static const std::array<vk::DescriptorPoolSize, 1> POOL_SIZES;
 
   const GlfwWindow *window;
   vk::raii::Context context;
@@ -139,6 +178,8 @@ class Renderer {
   std::vector<Attachment> attachments;
   RenderPass renderPass;
   vk::raii::CommandPool cmdPool;
+  vk::raii::DescriptorPool descriptorPool;
+
   std::vector<RenderTarget> targets;
   std::vector<Frame> frames;
 

seng/include/seng/utils.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
 #include <string>
 #include <vector>
 
@@ -8,19 +11,32 @@ namespace seng::internal {
 /**
  * Read the contents of the file with the given path in an array of bytes.
  */
-extern std::vector<char> readFile(const std::string& name);
+extern std::vector<char> readFile(const std::string &name);
 
 /**
  * Create a vector containing n in-place created objects
  */
 template <typename T, typename... Args>
-std::vector<T> many(typename std::vector<T>::size_type n, Args&&... args) {
+std::vector<T> many(typename std::vector<T>::size_type n, Args &&...args)
+{
   std::vector<T> ret;
   ret.reserve(n);
   for (typename std::vector<T>::size_type i = 0; i < n; i++) {
     ret.emplace_back(args...);
   }
   return ret;
+}
+
+/**
+ * Helper function to combine a given hash with a generated hash for the input param.
+ *
+ * Taken straight out of Vulkan-Samples's `framework/common/helper.h`
+ */
+template <class T>
+inline void hashCombine(size_t &seed, const T &v)
+{
+  std::hash<T> hasher;
+  glm::detail::hash_combine(seed, hasher(v));
 }
 
 }  // namespace seng::internal

seng/src/rendering/renderer.cpp
@@ -79,11 +79,13 @@ Image Renderer::RenderTarget::createDepthBuffer(const Device &device, vk::Extent
 }
 
 // Definitions for Frame
+
 Renderer::Frame::Frame(const Device &device, const vk::raii::CommandPool &pool) :
     commandBuffer(device, pool, true),
     imageAvailableSem(device.logical(), vk::SemaphoreCreateInfo{}),
     queueCompleteSem(device.logical(), vk::SemaphoreCreateInfo{}),
     inFlightFence(device, true),
+    descriptorSets(),
     imageIndex(-1)
 {
   log::info("Allocated resources for a frame");
@@ -104,6 +106,9 @@ void FrameHandle::invalidate()
 static constexpr vk::CommandPoolCreateFlags cmdPoolFlags =
     vk::CommandPoolCreateFlagBits::eResetCommandBuffer;
 
+static constexpr vk::DescriptorPoolCreateFlags descriptorPoolFlags =
+    vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet;
+
 static constexpr vk::BufferUsageFlags vertexBufferUsage =
     vk::BufferUsageFlagBits::eVertexBuffer | vk::BufferUsageFlagBits::eTransferSrc |
     vk::BufferUsageFlagBits::eTransferDst;
@@ -113,6 +118,11 @@ static constexpr vk::BufferUsageFlags indexBufferUsage =
 
 const std::vector<const char *> Renderer::VALIDATION_LAYERS{
     "VK_LAYER_KHRONOS_validation"};
+
+// TODO: add other sizes
+const std::array<vk::DescriptorPoolSize, 1> Renderer::POOL_SIZES{{
+    {vk::DescriptorType::eUniformBuffer, 1024},
+}};
 
 Renderer::Renderer(ApplicationConfig config, const GlfwWindow &window) :
     window(std::addressof(window)),
@@ -147,6 +157,13 @@ Renderer::Renderer(ApplicationConfig config, const GlfwWindow &window) :
     // Pools
     cmdPool(device.logical(),
             {cmdPoolFlags, *device.queueFamilyIndices().graphicsFamily}),
+    descriptorPool(std::invoke([&]() {
+      vk::DescriptorPoolCreateInfo info{};
+      info.flags = descriptorPoolFlags;
+      info.maxSets = 1024;  // FIXME: not really sure about this
+      info.setPoolSizes(Renderer::POOL_SIZES);
+      return vk::raii::DescriptorPool(device.logical(), info);
+    })),
 
     // Buffers
     vertexBuffer(device, vertexBufferUsage, sizeof(Vertex) * 1024 * 1024),
@@ -237,6 +254,40 @@ void Renderer::signalResize()
   fbGeneration++;
 }
 
+void Renderer::requestDescriptorSet(vk::DescriptorSetLayout layout)
+{
+  // Check if any of the frames do not have the layout registered.
+  // We can also "optimize" and check only one since we always keep them in sync,
+  // but just to be safe from inconsistent states.
+  bool notFound = false;
+  for (auto &f : frames) {
+    auto iter = f.descriptorSets.find(layout);
+    if (iter == f.descriptorSets.end()) {
+      notFound = true;
+      break;
+    }
+  }
+  // If the descriptor layout has not been founmd in some frames, allocate the
+  // map entries
+  if (notFound) {
+    for (auto &f : frames) {
+      array<vk::DescriptorSetLayout, 1> descs{layout};
+
+      vk::DescriptorSetAllocateInfo info{};
+      info.descriptorPool = *descriptorPool;
+      info.setSetLayouts(descs);
+      auto sets = vk::raii::DescriptorSets(device.logical(), info);
+      f.descriptorSets.emplace(layout, std::move(sets[0]));
+    }
+  }
+}
+
+void Renderer::clearDescriptorSets()
+{
+  for (auto &f : frames) f.descriptorSets.clear();
+  descriptorPool.reset();
+}
+
 optional<FrameHandle> Renderer::beginFrame()
 {
   if (recreatingSwapchain) {
@@ -283,6 +334,13 @@ optional<FrameHandle> Renderer::beginFrame()
     log::warning("Caught exception: {}", e.what());
     return nullopt;
   }
+}
+
+const vk::raii::DescriptorSet &Renderer::getDescriptorSet(
+    const FrameHandle &handle, vk::DescriptorSetLayout layout) const
+{
+  if (handle.invalid(frames.size())) throw runtime_error("Invalid handle passed");
+  return frames[handle.frameIndex].descriptorSets.at(layout);
 }
 
 // FIXME: start of stub

commit dc96ce84f99aa5c5715a94e061c429d42d180c60

fix(Renderer): fix vk::DescriptorSetLayout hash
Fixes: 0ad8f81a15159654fc0819db635eaa3ce98d31f8

seng/include/seng/rendering/renderer.hpp
@@ -30,7 +30,8 @@ struct hash<vk::DescriptorSetLayout> {
   std::size_t operator()(const vk::DescriptorSetLayout &descriptorSetLayout) const
   {
     std::size_t result = 0;
-    seng::internal::hashCombine(result, descriptorSetLayout);
+    seng::internal::hashCombine(result,
+                                static_cast<VkDescriptorSetLayout>(descriptorSetLayout));
     return result;
   }
 };