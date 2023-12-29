526957b65ed274801aebd4b6b1ebc6bdbf42ee95
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-12-30 00:27:22 +0100

    feat(ToggleComponent): add ToggleComponent
    This new abstract component implements enable/disable behaviour

---
+++ seng/include/seng/components/toggle.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <seng/components/base_component.hpp>
+#include <seng/components/definitions.hpp>
+
+namespace seng::components {
+
+/**
+ * Add support for enabling/disabling.
+ */
+class ToggleComponent : public BaseComponent {
+ public:
+  /// Constructor
+  ToggleComponent(scene::Entity &entity, bool enabled = true);
+  ToggleComponent(const ToggleComponent &) = delete;
+  ToggleComponent(ToggleComponent &&) = delete;
+
+  ToggleComponent &operator=(const ToggleComponent &) = delete;
+  ToggleComponent &operator=(ToggleComponent &&) = delete;
+
+  DECLARE_COMPONENT_ID("ToggleComponent");
+
+  // Accessors
+  bool enabled() const { return m_enabled; }
+  void enabled(bool b);
+
+  /**
+   * Enable the Component. `onEnable` is called.
+   */
+  void enable();
+
+  /**
+   * Disable the Component. `onDisable` is called.
+   */
+  void disable();
+
+  /**
+   * Toggle the Component. `onDisable` or `onEnable` are called depending on
+   * the state.
+   */
+  void toggle();
+
+  /**
+   * Hook to execute code when the Component is enabled.
+   */
+  virtual void onEnable() {}
+
+  /**
+   * Hook to execute code when the Component is disabled.
+   */
+  virtual void onDisable() {}
+
+ private:
+  bool m_enabled;
+};
+
+}  // namespace seng::components
---
+++ seng/src/components/toggle.cpp
@@ -0,0 +1,35 @@
+#include <seng/components/base_component.hpp>
+#include <seng/components/definitions.hpp>
+#include <seng/components/toggle.hpp>
+
+using namespace seng::components;
+
+ToggleComponent::ToggleComponent(scene::Entity &entity, bool enabled) :
+    BaseComponent(entity)
+{
+  m_enabled = enabled;
+}
+
+void ToggleComponent::enabled(bool b)
+{
+  m_enabled = b;
+  if (m_enabled)
+    onEnable();
+  else
+    onDisable();
+}
+
+void ToggleComponent::enable()
+{
+  enabled(true);
+}
+
+void ToggleComponent::disable()
+{
+  enabled(false);
+}
+
+void ToggleComponent::toggle()
+{
+  enabled(!m_enabled);
+}
