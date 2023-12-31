55bbf11318f321bedcef9fff263fe262d4b07ec4
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2023-12-31 20:07:09 +0100

    style(Application): reorder methods

--- seng/include/seng/application.hpp
+++ seng/include/seng/application.hpp
@@ -35,6 +35,13 @@ class Application {
   Application &operator=(const Application &other) = delete;
   Application &operator=(const Application &&other) noexcept = delete;
 
+  // Accessors
+  const ApplicationConfig &config() const { return conf; }
+  const std::unique_ptr<rendering::Renderer> &renderer() const { return m_vulkan; }
+  const std::unique_ptr<rendering::GlfwWindow> &window() const { return m_glfwWindow; }
+  const std::unique_ptr<scene::Scene> &currentScene() const { return m_scene; }
+  const std::unique_ptr<InputManager> &input() const { return m_inputManager; }
+
   /**
    * Starts execution of the engine in a window of the specified starting size.
    * Blocks until application is closed.
@@ -48,12 +55,6 @@ class Application {
    */
   void stop();
 
-  // Accessors
-  const ApplicationConfig &config() const { return conf; }
-  const std::unique_ptr<rendering::Renderer> &renderer() const { return m_vulkan; }
-  const std::unique_ptr<rendering::GlfwWindow> &window() const { return m_glfwWindow; }
-  const std::unique_ptr<scene::Scene> &currentScene() const { return m_scene; }
-  const std::unique_ptr<InputManager> &input() const { return m_inputManager; }
 
  private:
   ApplicationConfig conf;
